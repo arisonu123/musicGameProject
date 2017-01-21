1ab9fc860e7b4efbf329444a08afce69daca24a1
Alison Taylor <taylorakt1993@gmail.com>
2017-01-20 22:30:34 -0800

    Work on UI
    Created UIManager and started work on card prefab. Started work on title
    screen and main menu

---
+++ Assets/Scripts/Managers/GameMaster.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	// Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+
+    private void startGame()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+
+    public void dragCard()
+    {
+
+    }
+
+    public void dropCard()
+    {
+
+    }
+
+    public void playSong()
+    {
+      //  GameObject.FindWithTag("songObj").GetComponent<SongClass>().song.PlayClip();
+    }
+}
