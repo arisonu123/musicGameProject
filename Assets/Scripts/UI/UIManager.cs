a7eec3d1a8e53fcadd67b954b5450b173f208ac8
Alison Taylor <taylorakt1993@gmail.com>
2017-01-21 00:32:36 -0800

    Ui stuff
    -More UI functionality written
    -Card class made

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,6 +3,19 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
+    private GameObject[] songCards;
+
+    /// <summary>
+    /// Array of cards
+    /// </summary>
+    public GameObject[] songCardArray
+    {
+        get { return songCards; }
+
+        set { songCards = value; }
+    }
+
+    private int[] currentDisplayedCardNums = { 0, 1, 2, 3 };
 	// Use this for initialization
 	private void Start () {
 	
@@ -20,11 +33,52 @@ public class UIManager : MonoBehaviour {
 
     public void dropCard()
     {
+        //fill data in this spot to correspond to data of dropped card
+
 
     }
 
+    /// <summary>
+    /// Play the song
+    /// </summary>
     public void playSong()
     {
       //  GameObject.FindWithTag("songObj").GetComponent<SongClass>().song.PlayClip();
+    }
+
+    /// <summary>
+    /// Replay the level
+    /// </summary>
+    public void replayLevel()
+    {
+        //replay level
+    }
+
+    /// <summary>
+    /// Rotate the cards up
+    /// </summary>
+    public void rotateCardsUp()
+    {
+        for(int i = 0; i < currentDisplayedCardNums.Length; i++)
+        {
+            songCards[currentDisplayedCardNums[i]].SetActive(false);
+            currentDisplayedCardNums[i] += 1;
+            songCards[currentDisplayedCardNums[i]].SetActive(true);
+        }     
+       
+    }
+
+
+    /// <summary>
+    /// Rotate the cards down
+    /// </summary>
+    public void rotateCardsDown()
+    {
+        for (int i = 0; i < currentDisplayedCardNums.Length; i++)
+        {
+            songCards[currentDisplayedCardNums[i]].SetActive(false);
+            currentDisplayedCardNums[i] -= 1;
+            songCards[currentDisplayedCardNums[i]].SetActive(true);
+        }
     }
 }
---
+++ Assets/Scripts/cardClass.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class cardClass : MonoBehaviour {
+#pragma warning disable 649
+    [Header("Possible Card Images")]
+    [SerializeField]
+    [Tooltip("Images to display on the cards")]
+    private Image[] cardImages=new Image[8];
+
+    [Header("Card specifics")]
+    [SerializeField]
+    [Range(0,8)]
+    [Tooltip("This number determines the image loaded onto the card, The image will be the image at this location from the card image array")]
+    private int cardNumber;
+    [SerializeField]
+    [Tooltip("Determines whether or not this card is a player card or a card to match to")]
+    private bool isPlayerCard;
+#pragma warning restore 649
+
+    private void Awake()
+    {
+        this.gameObject.GetComponent<Image>().sprite = cardImages[cardNumber].sprite;
+    }
+	// Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+}
