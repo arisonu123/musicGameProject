346aa5829eeb59850aee77a3983d086cbcd3ca3b
snorem <shaun.norem@gmail.com>
2017-01-21 00:48:53 -0800

    Added functions
    Song Class - References to sound file and integer array for notes.
    
    Succeed/Fail functions (incomplete) and checking for correct placement
    of notes

---
+++ Assets/Scripts/SongClass.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongClass : MonoBehaviour {
+
+	[SerializeField]
+	[Header("Song File")]
+	private AudioClip song;
+
+	/// <summary>
+	/// Gets the song clip associated with this class
+	/// </summary>
+	/// <value>The song clip.</value>
+	public AudioClip songClip{
+		get{ return song;}
+	}
+
+	[SerializeField]
+	[Header("Song Notes")]
+	private int[] notes;
+
+	/// <summary>
+	/// Gets the note value in the select portion of the song (0 = first)
+	/// </summary>
+	/// <value>The song clip.</value>
+	public int getNote(int position)
+	{
+		return notes[position];
+	}
+
+	public int getSongLength()
+	{
+		return notes.Length;
+	}
+}
+
+
---
+++ Assets/Scripts/WinLoseCondition.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLoseCondition : MonoBehaviour {
+
+	//The following would be in scene manager, probably with these functions as well
+	int currentLevel; // Fr referencing songList active song
+	GameObject[] songList; // List of all songs we have 
+	GameObject UI;
+	SongClass activeSongNotes;
+	UIManager placedNotes;
+
+	private void setNoteReferences()
+	{
+		activeSongNotes = songList[currentLevel].GetComponent<SongClass>();
+		placedNotes = UI.GetComponent<UIManager>();
+	}
+
+	/// <summary>
+	/// Compares the notes of the song to the notes placed by the players
+	/// </summary>
+	public void compareNotes()
+	{
+		int songLength = activeSongNotes.getSongLength();
+
+		for(int i = 0; i < songLength; i++)
+		{
+			if(!(activeSongNotes.getNote[i] & placedNotes.songCardArray[i])) onFail(); 
+			else if (i == songLength - 1) onSuccess(); // Last one, all match, success!
+		}
+	}
+
+	/// <summary>
+	/// Loads next level if players successfully match notes
+	/// </summary>
+	private void onSuccess()
+	{
+		// Load data for next level (song, length)
+		// Adjust UI as needed
+
+	}
+
+	/// <summary>
+	/// Resets level if players fail to match notes
+	/// </summary>
+	private void onFail()
+	{
+		// Reset level, return cards to pool and redistribute to players
+
+	}
+}
