0bfc5ab223a542fc129f86920725b806eb01314a
Alexia Mandeville <al@alexiamandeville.com>
2017-01-21 10:11:09 -0800

    updating room connection

--- Assets/Scripts/Managers/NetworkManager.cs
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -6,36 +6,31 @@ public class NetworkManager : MonoBehaviour {
 	private const string roomName = "RoomName";
 	private RoomInfo[] roomsList;
 
+	bool joined;
+
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("0.1");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!PhotonNetwork.connected)
-		{
-			//GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
-		}
-		else if (PhotonNetwork.room == null)
-		{
-			// Create Room
-				PhotonNetwork.CreateRoom(roomName);
-
-			// Join Room
-			if (roomsList != null)
-			{
-				for (int i = 0; i < roomsList.Length; i++)
-				{
-						PhotonNetwork.JoinRoom(roomsList[i].Name);
-				}
-			}
-		}
 	}
 
 	void OnReceivedRoomListUpdate()
 	{
 		roomsList = PhotonNetwork.GetRoomList();
+	}
+
+	void OnJoinedLobby(){
+		//create room
+		if (PhotonNetwork.room == null) {
+			PhotonNetwork.CreateRoom (roomName);
+		}
+
+		// Join Room
+		if (roomsList != null) {
+			for (int i = 0; i < roomsList.Length; i++) {
+				PhotonNetwork.JoinRoom (roomsList [i].Name);
+			}
+
+		}
 	}
 
 	void OnJoinedRoom()
