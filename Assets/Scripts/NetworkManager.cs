d06933e04aa72c929277a589620cfa175793a9ae
Alexia Mandeville <al@alexiamandeville.com>
2017-01-20 21:10:08 -0800

    networking manager

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string roomName = "RoomName";
+	private RoomInfo[] roomsList;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!PhotonNetwork.connected)
+		{
+			//GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		}
+		else if (PhotonNetwork.room == null)
+		{
+			// Create Room
+				PhotonNetwork.CreateRoom(roomName);
+
+			// Join Room
+			if (roomsList != null)
+			{
+				for (int i = 0; i < roomsList.Length; i++)
+				{
+						PhotonNetwork.JoinRoom(roomsList[i].Name);
+				}
+			}
+		}
+	}
+
+	void OnReceivedRoomListUpdate()
+	{
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+
+	void OnJoinedRoom()
+	{
+		Debug.Log("Connected to Room");
+	}
+}
